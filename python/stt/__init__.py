7086f2971ca7f9872aa83bed92218ea1b5fd9005
JoBe <165585785+JoBeGaming@users.noreply.github.com>
2025-09-06 22:16:41 +0200

    Updated style for readability.

--- python/stt/__init__.py
+++ python/stt/__init__.py
@@ -35,20 +35,30 @@ def _walk_tree(tree, /, *, level = 0):
     """
 
     final = []
+
+    # Make sure we have a tuple,
+    # so we can iterate over it.
     if not isinstance(tree, tuple):
         tree = (tree,)
+
+    # Special case
     if tree == (...,):
         return None
+
     for tp in tree:
         if tp is ...:
+
             for index_of_ellipsis_owner in range(len(final) - 1, -1, -1):
                 if final[index_of_ellipsis_owner][0] == level:
                     prev_args = final[index_of_ellipsis_owner][:-1]
                     final[index_of_ellipsis_owner] = (prev_args + (True,))
                     break
+
             continue
+
         origin = _get_node_origin(tp, allow_none=True, allow_all=True)
         args = _get_node_args(tp, allow_all=True)
+
         if origin is None:
             final.append((level, tp, False))
         else:
@@ -56,6 +66,7 @@ def _walk_tree(tree, /, *, level = 0):
             inner = _walk_tree(args, level=level + 1)
             if inner:
                 final.extend(inner)
+
     return tuple(final)
 
 
@@ -70,7 +81,12 @@ def _is_traversable(tree):
     try:
         _walk_tree(tree)
         return True
-    except (TypeError, KeyError, IndexError):
+
+    except (
+        TypeError, 
+        KeyError, 
+        IndexError
+    ):
         return False
 
 
@@ -85,20 +101,25 @@ def _is_reconstructed(tree):
 def _reconstruct(tree):
     if _is_reconstructed(tree):
         return tree
+
     final = ()
     counter = 0
     prev_level = 0
+
     for level, node, ellipsis_following in tree:
+
         if counter == 0:
             final += (node,)
             if ellipsis_following:
                 final += (...,)
+
         if level > prev_level:
                 ...
         elif level < prev_level:
                 ...
         else: 
                 ...
+
         counter += 1
 
 
@@ -149,6 +170,7 @@ def dump(tree, *, file=sys.stdout):
     # for making it iterable
 
     iterable_tree = _make_iterable(_reconstruct(tree))
+
     for node in iterable_tree:
         print(_reconstruct_node(node), file=file)
 
@@ -188,5 +210,6 @@ def dump_iterable(tree):
     # for making it iterable
 
     iterable_tree = _make_iterable(_reconstruct(tree))
+
     for node in iterable_tree:
         yield _reconstruct_node(node)
